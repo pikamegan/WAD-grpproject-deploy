8b3aea56bcd2c049cc00b531125743747612a3da
Megan Thong <meganthong@outlook.com>
2021-11-10 23:40:36 +0800

    Update ConnectionManager.php

--- processDbRequest/model/ConnectionManager.php
+++ processDbRequest/model/ConnectionManager.php
@@ -1,53 +1,10 @@
 <?php
-class ConnectionManager
-{
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-    public function getConnection()
-    {
-        $servername = 'localhost';
-        $username = 'root';
-        // $password = 'root'; // for Mac
-        $password = '';
-        $dbname = 'wad2g5t2intern';
-        $port = '3306';
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
-        // Create connection
-        $pdoObject = new PDO(
-            "mysql:host=$servername;dbname=$dbname;port=$port",
-            $username,
-            $password);
-
-        $pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // if fail, exception will be thrown
-
-        // this dont work
-        // $pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
-
-
-        // Return connection object
-        return $pdoObject; // PDO object (containing MySQL connection info)
-    }
-
-}
-
-
-
-// works for tim
-
-// class ConnectionManager
-// {
-//     public function getConnection()
-//     {
-//         $dsn  = "mysql:host=localhost;dbname=wad2g5t2intern";
-//         return new PDO($dsn, "root", "");
-//     }
-// }
-
+$conn = new mysqli($server, $username, $password, $db);
 ?>
-
-
-
-
-
-
-
