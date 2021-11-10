f11a2cf34b799101ef24e22a65a8f3be2c3c780e
Megan Thong <meganthong@outlook.com>
2021-11-10 23:48:55 +0800

    Update ConnectionManager.php

--- processDbRequest/model/ConnectionManager.php
+++ processDbRequest/model/ConnectionManager.php
@@ -1,10 +1,37 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+class ConnectionManager
+{
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+    public function getConnection()
+    {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$conn = new mysqli($server, $username, $password, $db);
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"], 1);
+
+        $conn = new mysqli($server, $username, $password, $db);
+
+        //$port = '3306';
+
+        // Create connection
+//         $pdoObject = new PDO(
+//             "mysql:host=$server;dbname=$db;port=$port",
+//             $username,
+//             $password);
+
+//         $pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // if fail, exception will be thrown
+
+        // this dont work
+        // $pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+
+        // Return connection object
+        return $conn; // PDO object (containing MySQL connection info)
+    }
+
+}
+
 ?>
